f10ad3d1e19f767693e5bf6762fde568502c8ff4
lofarusers user <twybouw@m1.iihe.ac.be>
2024-02-15 15:04:03 +0100

    class structure

--- NuRadioReco/modules/io/coreas/readCoREASInterpolator.py
+++ NuRadioReco/modules/io/coreas/readCoREASInterpolator.py
@@ -1,5 +1,21 @@
+from NuRadioReco.modules.base.module import register_run
 import numpy as np
 import h5py
 import NuRadioReco.framework.station
 from radiotools.coordinatesystems import cstrafo
 from NuRadioReco.utilities import units
+import cr_pulse_interpolator.signal_interpolation_fourier as sigF
+
+class readCoREASInterpolator():
+    def __init__(self) -> None:
+        pass
+
+    def begin():
+        pass
+    
+    @register_run()
+    def run():
+        pass
+
+    def end():
+        pass
