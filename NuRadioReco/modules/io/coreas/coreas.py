a929cd7300810e863e6ca5144ced4f2448eeaf21
twybo <tiepolo.wybouw@gmail.com>
2024-02-20 17:26:25 +0100

    convenience corsika observer to nrr data converter factored out make_sim_station

--- NuRadioReco/modules/io/coreas/coreas.py
+++ NuRadioReco/modules/io/coreas/coreas.py
@@ -122,8 +122,18 @@ def calculate_simulation_weights(positions, zenith, azimuth, site='summit', debu
 
     return weights
 
+def observer_to_nrr_data(observer: np.ndarray):
+    data = np.copy(observer)
+    data[:, 1], data[:, 2] = -observer[:, 2], observer[:, 1]
+    data[:, 0] *= units.second
+    data[:, 1] *= conversion_fieldstrength_cgs_to_SI
+    data[:, 2] *= conversion_fieldstrength_cgs_to_SI
+    data[:, 3] *= conversion_fieldstrength_cgs_to_SI
+    return data
 
-def make_sim_station(station_id, corsika, observer, channel_ids, weight=None):
+
+
+def make_sim_station(station_id, corsika, observer, channel_ids, weight=None, coreas_observer_format: bool = True):
     """
     creates an NuRadioReco sim station from the (interpolated) observer object of the coreas hdf5 file
 
@@ -137,6 +147,8 @@ def make_sim_station(station_id, corsika, observer, channel_ids, weight=None):
     channel_ids :
     weight : weight of individual station
         weight corresponds to area covered by station
+    coreas_observer_format: bool (defualt: True)
+        if `observer` argument is in the coreas cgs units and coordinates. If True, is passed to `coreas_observer_to_nrr_data` which switches x,y -> -y, x and converts to SI units. 
 
     Returns
     -------
@@ -148,16 +160,11 @@ def make_sim_station(station_id, corsika, observer, channel_ids, weight=None):
 
     if(observer is None):
         data = np.zeros((512, 4))
-        data[:, 0] = np.arange(0, 512) * units.ns / units.second
+        data[:, 0] = np.arange(0, 512) * units.ns 
+    elif coreas_observer_format:
+        data = observer_to_nrr_data(observer)
     else:
         data = np.copy(observer)
-        data[:, 1], data[:, 2] = -observer[:, 2], observer[:, 1]
-
-    # convert to SI units
-    data[:, 0] *= units.second
-    data[:, 1] *= conversion_fieldstrength_cgs_to_SI
-    data[:, 2] *= conversion_fieldstrength_cgs_to_SI
-    data[:, 3] *= conversion_fieldstrength_cgs_to_SI
 
     cs = coordinatesystems.cstrafo(zenith, azimuth, magnetic_field_vector=magnetic_field_vector)
     efield = cs.transform_from_magnetic_to_geographic(data[:, 1:].T)
